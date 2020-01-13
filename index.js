630862c5d42ead13f8aa372449c9e8472b135808
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-01-13 15:08:23 -0500

    setup index.js with express server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// implement your API here
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+const port = 8000;
+server.listen(port, () => console.log("API ONLINE"));
